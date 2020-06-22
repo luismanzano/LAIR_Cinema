44524b17708fd059db05d185d2362ce165c2d8e3
= <luisfmanzanoa@gmail.com>
2020-06-22 01:55:26 -0400

    Initialized Repo

---
+++ backend/lairvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\luis f. manzano\desktop\lair\backend\lairvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
